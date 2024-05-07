8ffc8ea85f9de0e37d59cf0b095963aa2eba1daa
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-05-08 11:06:50 +1200

    finished app

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 function addTodo() {
-    // Get the input element and its value
-    const input = document.getElementById(todo-input);
-    // Get value and trim white space from start/end
-    const inputValue = input.value.trim();
+  // Get the input element and its value
+  const input = document.getElementById("todo-input");
+  // Get value and trim whitespace from start/end
+  const inputValue = input.value.trim();
 
-    //check to see if the input value isnt empty
-    if (inputValue !== "") {
-        // Create new list item
-        // Get the unordered list
-        const todoList = document.getElementById("todo-list");
-        // Create a list item using createElement:
-        const li = document.createElement("li");
-        // create a checkbox using createElemnt:
-        const checkbox = document.createElement("input");
-        // Set checkbox input to a checkbox:
-        checkbox.type = "checkbox";
-        // Create a textNode that uses the inputValue
-        const textNode = document.createTextNode(inputValue);
+  // Check to se eif the input value isnt empty
+  if (inputValue !== "") {
+    // Create a new list item
+    // Get the unorder list:
+    const todoList = document.getElementById("todo-list");
+    // Create a list item using createElement:
+    const li = document.createElement("li");
+    // Create a checkbox using createElement:
+    const checkbox = document.createElement("input");
+    // Set checkbox input to a checkbox:
+    checkbox.type = "checkbox";
+    // Create a textNode that uses the inputvalue
+    const textNode = document.createTextNode(inputValue);
 
-        //Append checkbox and text to the list item:
-        li.appendChild(checkbox);
-        li.appendChild(textNode);
-        // Append the list item to the ul of todo list:
-        todoList.appendChild(li);
-    } else {
-        // Alert user if value is empty
-        alert("Please enter a valid todo item")
-    }
+    // Append checkbox and text to the list item:
+    li.appendChild(checkbox);
+    li.appendChild(textNode);
+    // Append the list item to the ul of todo list:
+    todoList.appendChild(li);
+  } else {
+    // Alert user if value is empty
+    alert("Please enter a valid todo item");
+  }
 }
+
+// Add event listener to the "Add button" to add new to item:
+document.getElementById('add-button').addEventListener("click", addTodo);
+
+// Event Listener to mark todo items as completed
+
+// Change event on the todo item
+document.getElementById("todo-list").addEventListener("change", function (event) {
+  // check if the event trigger was a checkbox
+  if (event.target.type === "checkbox") {
+    // Get the parent list item of the checkbox
+    const listItem = event.target.parentNode;
+    // check if the checkbox is checked or not:
+    if (event.target.checked) {
+      // Apply line-through style when the checkbox is checked
+      listItem.style.textDecoration = "line-through";
+    } else {
+      //Remove line-through when unchecked:
+      listItem.style.textDecoration = "none";
+    }
+  }
+})
