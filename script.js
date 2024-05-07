6370a2e4dd351b9e4c521b525e849276a224a9a0
BaxterMcKenzie <baxmckenzie@gmail.com>
2024-05-08 09:57:03 +1200

    wrote todo function

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('working')
+function addTodo() {
+    // Get the input element and its value
+    const input = document.getElementById(todo-input);
+    // Get value and trim white space from start/end
+    const inputValue = input.value.trim();
+
+    //check to see if the input value isnt empty
+    if (inputValue !== "") {
+        // Create new list item
+        // Get the unordered list
+        const todoList = document.getElementById("todo-list");
+        // Create a list item using createElement:
+        const li = document.createElement("li");
+        // create a checkbox using createElemnt:
+        const checkbox = document.createElement("input");
+        // Set checkbox input to a checkbox:
+        checkbox.type = "checkbox";
+        // Create a textNode that uses the inputValue
+        const textNode = document.createTextNode(inputValue);
+
+        //Append checkbox and text to the list item:
+        li.appendChild(checkbox);
+        li.appendChild(textNode);
+        // Append the list item to the ul of todo list:
+        todoList.appendChild(li);
+    } else {
+        // Alert user if value is empty
+        alert("Please enter a valid todo item")
+    }
+}
